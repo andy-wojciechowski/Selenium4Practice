cae3ef24534af3e994f80e5ba83de36ce39dfd58
andy-wojciechowski <andy052799@gmail.com>
2021-11-14 08:58:14 -0600

    Initial implementation of the screenshot handler

--- Selenium4Practice.Framework/TestAttachments/SeleniumScreenshotTestAttachmentHandler .cs
+++ Selenium4Practice.Framework/TestAttachments/SeleniumScreenshotTestAttachmentHandler .cs
@@ -1,6 +1,10 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using Selenium4Practice.Framework.TestAttachments.Interfaces;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Selenium4Practice.Framework.TestAttachments;
 
@@ -8,6 +12,12 @@ public class SeleniumScreenshotTestAttachmentHandler : ISeleniumScreenshotTestAt
 {
     public void Execute(TestContext context, IWebDriver webDriver)
     {
-        throw new System.NotImplementedException();
+        if (context.Result.Outcome == ResultState.Failure || context.Result.Outcome == ResultState.Error)
+        {
+            var screenshot = (ITakesScreenshot)webDriver;
+            var filePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\{context.Test.MethodName} {Guid.NewGuid()}.png";
+            screenshot.GetScreenshot().SaveAsFile(filePath, ScreenshotImageFormat.Png);
+            TestContext.AddTestAttachment(filePath);
+        }
     }
 }
