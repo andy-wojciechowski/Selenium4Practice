f7ed710f636a2468dfec24a5645a18de54ec9ad7
andy-wojciechowski <andy052799@gmail.com>
2021-11-18 12:45:15 -0600

    Implemented the JavaScriptLogsTestAttachmentHandler

--- Selenium4Practice.Framework/TestAttachments/JavaScriptLogsTestAttachmentHandler.cs
+++ Selenium4Practice.Framework/TestAttachments/JavaScriptLogsTestAttachmentHandler.cs
@@ -1,6 +1,12 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using Selenium4Practice.Framework.TestAttachments.Interfaces;
+using Serilog;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Selenium4Practice.Framework.TestAttachments;
 
@@ -15,6 +21,13 @@ public class JavaScriptLogsTestAttachmentHandler : IJavaScriptLogsTestAttachment
 
     public void Execute(TestContext context)
     {
-        throw new System.NotImplementedException();
+        if (context.Result.Outcome == ResultState.Failure || context.Result.Outcome == ResultState.Error)
+        {
+            var jsErrors = _webDriver.Manage().Logs.GetLog(LogType.Browser).Where(x => x.Level == LogLevel.Severe).ToList();
+            var logFileName = $"JS Errors {context.Test.MethodName} {Guid.NewGuid()}.txt";
+            var log = new LoggerConfiguration().WriteTo.File(logFileName).CreateLogger();
+            jsErrors.ForEach(x => log.Information(x.Message));
+            var fullFilePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\{logFileName}";
+        }
     }
 }
