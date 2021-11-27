144cbbe98a5aec096808942ac0ec65f29b2ae809
andy-wojciechowski <andy052799@gmail.com>
2021-11-26 19:20:30 -0600

    Adding the SignUpModal

---
+++ Selenium4Practice.Pages/Modals/SignUpModal.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using Selenium4Practice.Framework.Extensions;
+
+namespace Selenium4Practice.Pages.Modals;
+
+public class SignUpModal : BaseDemoblazeModal
+{
+    #region Properties
+
+    public override By Trait => By.Id("signInModal");
+    private By UserNameTextboxLocator => By.Id("loginusername");
+    private By PasswordTextboxLocator => By.Id("loginpassword");
+    private By LoginButtonLocator => By.ClassName("btn-primary");
+
+    public IWebElement UserNameTextbox => ModalElement.FindElement(UserNameTextboxLocator);
+    public IWebElement PasswordTextbox => ModalElement.FindElement(PasswordTextboxLocator);
+    public IWebElement LoginButton => ModalFooter.FindElement(LoginButtonLocator);
+
+    #endregion
+
+    #region Public Methods
+
+    public SignUpModal WithUserName(string userName)
+    {
+        UserNameTextbox.EnterTextInInput(userName);
+        return this;
+    }
+
+    public SignUpModal WithPassword(string password)
+    {
+        PasswordTextbox.EnterTextInInput(password);
+        return this;
+    }
+
+    public string LogIn(bool alertPresentAfterLogin = false)
+    {
+        LoginButton.Click();
+        if (alertPresentAfterLogin)
+        {
+            var alert = WebDriver.SwitchTo().Alert();
+            var alertText = alert.Text;
+            alert.Accept();
+            WaitForModalToBeClosed();
+            return alertText;
+        }
+        WaitForModalToBeClosed();
+        return null;
+    }
+
+    #endregion
+}
