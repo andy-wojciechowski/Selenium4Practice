31c992f43c701642af4c6dd876ceb7fe8ce3dfaf
andy-wojciechowski <andy052799@gmail.com>
2021-11-29 17:52:01 -0600

    Adding elements for the PlaceOrderModal

--- Selenium4Practice.Pages/Modals/PlaceOrderModal.cs
+++ Selenium4Practice.Pages/Modals/PlaceOrderModal.cs
@@ -1,5 +1,4 @@
 ﻿using OpenQA.Selenium;
-using System;
 
 namespace Selenium4Practice.Pages.Modals;
 
@@ -7,7 +6,22 @@ public class PlaceOrderModal : BaseDemoblazeModal
 {
     #region Properties
 
-    public override By Trait => throw new NotImplementedException();
+    public override By Trait => By.Id("orderModal");
+    private By NameTextboxLocator => By.Id("name");
+    private By CountryTextboxLocator => By.Id("country");
+    private By CityTextboxLocator => By.Id("city");
+    private By CreditCardTextboxLocator => By.Id("card");
+    private By MonthTextboxLocator => By.Id("month");
+    private By YearTextboxLocator => By.Id("year");
+    private By PurchaseButtonLocator => By.ClassName("btn-primary");
+
+    public IWebElement NameTextbox => ModalElement.FindElement(NameTextboxLocator);
+    public IWebElement CountryTextbox => ModalElement?.FindElement(CountryTextboxLocator);
+    public IWebElement CityTextbox => ModalElement.FindElement(CityTextboxLocator);
+    public IWebElement CreditCardTextbox => ModalElement.FindElement(CreditCardTextboxLocator);
+    public IWebElement MonthTextbox => ModalElement.FindElement(MonthTextboxLocator);
+    public IWebElement YearTextbox => ModalElement.FindElement(YearTextboxLocator);
+    public IWebElement PurchaseButton => ModalFooter.FindElement(PurchaseButtonLocator);
 
     #endregion
 }
