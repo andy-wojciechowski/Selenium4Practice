5414a1879fec0ec7bc78aa7609f9ab4b195eb2ac
andy-wojciechowski <andy052799@gmail.com>
2021-11-29 18:23:10 -0600

    Implemented PlaceOrderConfirmationModal

--- Selenium4Practice.Pages/Modals/PlaceOrderConfirmationModal.cs
+++ Selenium4Practice.Pages/Modals/PlaceOrderConfirmationModal.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using Selenium4Practice.Framework.Extensions;
 using Selenium4Practice.Pages.Infrastructure;
 
 namespace Selenium4Practice.Pages.Modals;
@@ -7,7 +8,21 @@ public class PlaceOrderConfirmationModal : BaseModal
 {
     #region Properties
 
-    public override By Trait => throw new System.NotImplementedException();
+    public override By Trait => By.ClassName("sweet-alert");
+    private By SuccessIconLocator => By.ClassName("sa-success");
+    private By TextLocator => By.ClassName("text-muted");
+    private By ConfirmButtonLocator => By.ClassName("confirm");
+
+    public IWebElement ModalElement => WebDriver.FindElement(Trait);
+    public IWebElement SuccessIcon => ModalElement.FindElement(SuccessIconLocator);
+    public IWebElement Text => ModalElement.FindElement(TextLocator);
+    public IWebElement ConfirmButton => ModalElement.FindElement(ConfirmButtonLocator);
+
+    #endregion
+
+    #region Public Methods
+
+    public void Confirm() => ConfirmButton.Click(WaitForModalToBeClosed);
 
     #endregion
 }
